e4f2f0dc2a42d2504fb70b7dc38e520eefc8f757
Rafa Gayoso <gayoso0597@gmail.com>
2022-02-26 22:52:57 +0100

    Seventh lecture completed

--- src/test/java/lectures/Lecture7.java
+++ src/test/java/lectures/Lecture7.java
@@ -2,9 +2,12 @@ package lectures;
 
 
 import beans.Car;
+import beans.Person;
+import com.google.common.collect.ImmutableList;
 import java.math.BigDecimal;
 import java.util.DoubleSummaryStatistics;
 import java.util.List;
+import java.util.OptionalDouble;
 import mockdata.MockData;
 import org.junit.Test;
 
@@ -12,35 +15,60 @@ public class Lecture7 {
 
   @Test
   public void count() throws Exception {
+    long count = MockData.getPeople()
+        .stream()
+        .filter(person -> person.getGender().equalsIgnoreCase("female"))
+        .count();
 
+    System.out.println(count);
   }
 
   @Test
   public void min() throws Exception {
+    double min = MockData.getCars()
+        .stream()
+        .filter(car -> car.getColor().equalsIgnoreCase("yellow"))
+        .mapToDouble(Car::getPrice)
+        .min()
+        .orElse(0);
+
+    System.out.println(min);
 
   }
 
   @Test
   public void max() throws Exception {
+    double max = MockData.getCars()
+        .stream()
+        .filter(car -> car.getColor().equalsIgnoreCase("yellow"))
+        .mapToDouble(Car::getPrice)
+        .max()
+        .orElse(0);
 
+    System.out.println(max);
   }
 
 
   @Test
   public void average() throws Exception {
-    List<Car> cars = MockData.getCars();
+    double average = MockData.getCars()
+        .stream()
+        .mapToDouble(Car::getPrice)
+        .average()
+        .orElse(0);
+
+    System.out.println(average);
 
   }
 
   @Test
   public void sum() throws Exception {
-    List<Car> cars = MockData.getCars();
-    double sum = cars.stream()
+    double sum = MockData.getCars()
+        .stream()
         .mapToDouble(Car::getPrice)
         .sum();
-    BigDecimal bigDecimalSum = BigDecimal.valueOf(sum);
-    System.out.println(sum);
-    System.out.println(bigDecimalSum);
+    BigDecimal bigDecimal = BigDecimal.valueOf(sum);
+    System.out.println(bigDecimal);
 
   }
 
@@ -50,12 +78,14 @@ public class Lecture7 {
     DoubleSummaryStatistics statistics = cars.stream()
         .mapToDouble(Car::getPrice)
         .summaryStatistics();
+
     System.out.println(statistics);
     System.out.println(statistics.getAverage());
     System.out.println(statistics.getCount());
     System.out.println(statistics.getMax());
     System.out.println(statistics.getMin());
     System.out.println(statistics.getSum());
+
   }
 
 }
