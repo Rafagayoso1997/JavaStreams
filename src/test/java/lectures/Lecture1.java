49b8bfe2612f2eb2d232c749c3b7d3cbfc80df67
Rafa Gayoso <gayoso0597@gmail.com>
2022-02-26 16:50:49 +0100

    First lecture complete

--- src/test/java/lectures/Lecture1.java
+++ src/test/java/lectures/Lecture1.java
@@ -2,8 +2,10 @@ package lectures;
 
 import beans.Person;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 import mockdata.MockData;
 import org.junit.Test;
 
@@ -15,12 +17,32 @@ public class Lecture1 {
     List<Person> people = MockData.getPeople();
     // 1. Find people aged less or equal 18
     // 2. Then change implementation to find first 10 people
+    List<Person> youngPeople = Lists.newArrayList();
+
+    final int limit = 10;
+    int counter = 0;
+    for (Person person : people) {
+      if (person.getAge() <= 18) {
+        youngPeople.add(person);
+        counter++;
+        if (counter == limit) {
+          break;
+        }
+      }
+    }
+
+    for (Person young : youngPeople) {
+      System.out.println(young);
+    }
 
   }
 
   @Test
   public void declarativeApproachUsingStreams() throws Exception {
-    ImmutableList<Person> people = MockData.getPeople();
-
+    MockData.getPeople().stream()
+        .filter(person -> person.getAge() <= 18)
+        .limit(10)
+        .collect(Collectors.toList())
+        .forEach(System.out::println);
   }
 }
