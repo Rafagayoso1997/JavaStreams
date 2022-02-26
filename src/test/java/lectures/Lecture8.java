6627c0f093998399e3c7386c4a1eeec74fbf9845
Rafa Gayoso <gayoso0597@gmail.com>
2022-02-26 23:13:51 +0100

    Eigth lecture completed

--- src/test/java/lectures/Lecture8.java
+++ src/test/java/lectures/Lecture8.java
@@ -1,7 +1,14 @@
 package lectures;
 
 
+import beans.Car;
+import beans.Person;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import mockdata.MockData;
 import org.assertj.core.util.Lists;
 import org.junit.Test;
 
@@ -9,12 +16,21 @@ public class Lecture8 {
 
   @Test
   public void simpleGrouping() throws Exception {
+    Map<String, List<Car>> grouping = MockData.getCars()
+        .stream()
+        .collect(Collectors.groupingBy(Car::getMake));
+
+    grouping.forEach((make, cars) -> {
+      System.out.println(make);
+      cars.forEach(System.out::println);
+    });
+
 
   }
 
   @Test
   public void groupingAndCounting() throws Exception {
-    ArrayList<String> names = Lists
+    /*ArrayList<String> names = Lists
         .newArrayList(
             "John",
             "John",
@@ -25,9 +41,17 @@ public class Lecture8 {
             "Vincent",
             "Alex",
             "Alex"
-        );
+        );*/
 
 
+    //Counting how many times an element appears on a list
+    Map<String, Long> counting = MockData.getPeople()
+        .stream()
+        .map(Person::getFirstName)
+        .limit(150)
+        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+    counting.forEach((name, count) -> System.out.println(name + " > " + count));
   }
 
 }
