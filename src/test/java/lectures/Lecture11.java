d7c3140834f1f49a254eba1426946489be6854cd
Rafa Gayoso <gayoso0597@gmail.com>
2022-02-27 09:33:50 +0100

    Eleventh lecture completed

--- src/test/java/lectures/Lecture11.java
+++ src/test/java/lectures/Lecture11.java
@@ -2,6 +2,7 @@ package lectures;
 
 import com.google.common.collect.ImmutableList;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.Test;
 
 public class Lecture11 {
@@ -9,12 +10,17 @@ public class Lecture11 {
   @Test
   public void joiningStrings() throws Exception {
     List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
-
+    String namesConcat = String.join(",", names);
+    System.out.println(namesConcat);
   }
 
   @Test
   public void joiningStringsWithStream() throws Exception {
     List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
+    String join = names.stream()
+        .map(String::toUpperCase)
+        .collect(Collectors.joining(","));
 
+    System.out.println(join);
   }
 }
